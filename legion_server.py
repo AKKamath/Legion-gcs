f313c08883124398a9ab2677a5676abf1c50d0c9
Aditya K Kamath <akamath1997@gmail.com>
2026-09-13 22:31:19 +0000

    Add support for LLVM bin

--- legion_server.py
+++ legion_server.py
@@ -113,7 +113,9 @@ def Run(args):
     # get the Legion_home path
     Legion_home = os.path.dirname(current_file_path)
 
-    server_path = os.path.join(Legion_home, "sampling_server/build/bin/sampling_server {} {}").format(gpu_number, cache_agg_mode)
+    # SAMPLING_SERVER_BIN: override which built binary to launch
+    server_bin = os.environ.get("SAMPLING_SERVER_BIN", os.path.join(Legion_home, "sampling_server/build/bin/sampling_server"))
+    server_path = "{} {} {}".format(server_bin, gpu_number, cache_agg_mode)
     nvbit_preload = os.environ.get("NVBIT_LD_PRELOAD")
     if nvbit_preload:
         server_path = "LD_PRELOAD={} {}".format(nvbit_preload, server_path)
