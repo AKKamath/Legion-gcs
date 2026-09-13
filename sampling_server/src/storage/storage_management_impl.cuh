4bfa103a85bdf8f5981ea00a04fb65b75fccbe3c
Aditya K Kamath <akamath1997@gmail.com>
2026-09-13 22:28:37 +0000

    Add missing returns on failing opening a file

--- sampling_server/src/storage/storage_management_impl.cuh
+++ sampling_server/src/storage/storage_management_impl.cuh
@@ -48,6 +48,7 @@ void mmap_trainingset_read(std::string &training_file, std::vector<int32_t>& tra
     int32_t fd = open(training_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<training_file<<"\n";
+        return;
     }
     // int64_t buf_len = lseek(fd, 0, SEEK_END);
     int64_t buf_len = int64_t(int64_t(training_set_ids.size()) * 4); 
@@ -68,6 +69,7 @@ int32_t mmap_partition_read(std::string &partition_file, int32_t* partition_inde
     int32_t fd = open(partition_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<partition_file<<"\n";
+        return fd;
     }
     int64_t buf_len = lseek(fd, 0, SEEK_END);
     const int32_t* buf = (int32_t *)mmap(NULL, buf_len, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -87,6 +89,7 @@ void mmap_indptr_read(std::string &indptr_file, int64_t* indptr){
     int32_t fd = open(indptr_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<indptr_file<<"\n";
+        return;
     }
     int64_t buf_len = lseek(fd, 0, SEEK_END);
     const int64_t *buf = (int64_t *)mmap(NULL, buf_len, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -106,6 +109,7 @@ void mmap_indices_read(std::string &indices_file, int32_t* indices){
     int32_t fd = open(indices_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<indices_file<<"\n";
+        return;
     }
     int64_t buf_len = lseek(fd, 0, SEEK_END);
     const int32_t *buf = (int32_t *)mmap(NULL, buf_len, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -125,6 +129,7 @@ void mmap_features_read(std::string &features_file, float* features){
     int32_t fd = open(features_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<features_file<<"\n";
+        return;
     }
     int64_t buf_len = lseek(fd, 0, SEEK_END);
     const float *buf = (float *)mmap(NULL, buf_len, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -144,6 +149,7 @@ void mmap_labels_read(std::string &labels_file, std::vector<int32_t>& labels){
     int32_t fd = open(labels_file.c_str(), O_RDONLY);
     if(fd == -1){
         std::cout<<"cannout open file: "<<labels_file<<"\n";
+        return;
     }
     int64_t buf_len = lseek(fd, 0, SEEK_END);
     const int32_t *buf = (int32_t *)mmap(NULL, buf_len, PROT_READ, MAP_PRIVATE, fd, 0);
